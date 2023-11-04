6d837067c65bd17e1530d7aa55d27048529aaeac
Muhammad Arifin <muhammad.arifin@nri.co.id>
2023-11-04 19:01:49 +0900

    feat: update OnboardingFinish

--- src/modules/home/components/Onboarding/OnboardingFinish.tsx
+++ src/modules/home/components/Onboarding/OnboardingFinish.tsx
@@ -1,16 +1,27 @@
 import checkMark from '@assets/images/check-mark.svg';
 import hanepyongJumping from '@assets/images/hanepyon-happy-jump.png';
-import landingBg from '@assets/images/landing.png';
 import { Overlay } from '@mantine/core';
 
-export default function OnboardingFinish() {
+interface Props {
+  text: string;
+  background: string;
+  withCheckMark?: boolean;
+}
+
+export default function OnboardingFinish({
+  text,
+  background,
+  withCheckMark,
+}: Props) {
   return (
     <div className="relative h-full w-full">
-      <div className="absolute z-[999] flex  h-full w-full flex-col items-center px-8 pt-20">
-        <div className="flex h-16 w-16 items-center justify-center rounded-full border-4 border-hanepyon-blue bg-hanepyon-yellow font-bold text-hanepyon-blue">
-          <img src={checkMark} alt="checkmark icon" />
+      {withCheckMark && (
+        <div className="absolute z-[999] flex  h-full w-full flex-col items-center px-8 pt-20">
+          <div className="flex h-16 w-16 items-center justify-center rounded-full border-4 border-hanepyon-blue bg-hanepyon-yellow font-bold text-hanepyon-blue">
+            <img src={checkMark} alt="checkmark icon" />
+          </div>
         </div>
-      </div>
+      )}
       <div className="absolute z-[800] flex h-full w-full flex-col items-center pt-[63px]">
         <img
           className="m-0 p-0"
@@ -19,10 +30,10 @@ export default function OnboardingFinish() {
           alt="Hanepyon jumping happily"
         />
         <h1 className="mt-8 text-center text-[2.5em] font-bold text-hanepyon-blue">
-          Well Done
+          {text}
         </h1>
       </div>
-      <img className="h-full w-full" src={landingBg} alt="Demo" />
+      <img className="h-full w-full" src={background} alt="Demo" />
       <Overlay color="#FFF" backgroundOpacity={0.5} blur={10} />
     </div>
   );
